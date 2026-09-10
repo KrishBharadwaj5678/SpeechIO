5fe4b930fd2ad5a3b4459d77b16e2fe2e1d4f36a
KrishBharadwaj5679 <krishbharadwaj43@gmail.com>
2026-09-10 18:03:20 +0530

    Add requirements.txt ✅

---
+++ speech_to_text.py
@@ -0,0 +1,20 @@
+# Speech To Text Convertor
+from speech_recognition import *
+
+# Create an object of Recognizer
+speechrecog=Recognizer()
+
+# Printing message on screen
+print("I am Listening You............")
+
+# using with statement which will automatically close the Microphone() class as m
+with Microphone() as m:
+
+    # Using listen() to Capture Microphone Input
+    audio=speechrecog.listen(m)
+
+    # here the listened voice will be converted into specified language
+    query=speechrecog.recognize_google(audio,language='eng')
+
+    # And at last we will print the voice converted into text
+    print(query)
